3aee2fce3d6936b6b415e06f807def96ab61e252
stoney <steenkamp@libralex.co.za>
2023-12-11 10:40:35 +0200

    WeatherApp

---
+++ WeatherMap/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using WeatherMap.Models;
+
+namespace WeatherMap.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private IConfiguration _configuration;
+
+		public HomeController(ILogger<HomeController> logger, IConfiguration config)
+		{
+			_logger = logger;
+			_configuration = config;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+
+		#region YourRegionName
+		// Your code goes here
+		[HttpGet]
+		[Route("/GetLocationData")]
+		public async Task<IActionResult> GetWeatherData(string location)
+		{
+			try
+			{
+
+				using (var client = new HttpClient())
+				{
+					string ApiKey = _configuration.GetValue<string>("WeatherApiKey");
+
+
+					var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.weatherapi.com/v1/current.json?key={ApiKey}&q={location}&aqi=no");
+					var response = await client.SendAsync(request);
+					response.EnsureSuccessStatusCode();
+
+					var responseBody = await response.Content.ReadAsStringAsync();
+
+					return Ok(
+						new { 
+							data = responseBody,
+							responseMessage = "success"
+					});
+				}
+
+			}catch (Exception ex)
+			{
+					return BadRequest(
+						new {  
+							data = "",
+							responseMessage = ex.Message 
+					});
+			}
+			
+			
+
+		}
+
+		#endregion
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ WeatherMap/wwwroot/js/site.js
@@ -0,0 +1,120 @@
+﻿let outputType = 1;
+
+var map = L.map('map').setView([-28, 25.19], 5);
+
+let locationData;
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '<p>Weather Demo</p>'
+}).addTo(map);
+
+function lookupLocation() {
+
+    try {
+       // debugger
+
+       console.log(outputType)
+
+        let location = $('#inputLocation').val();
+
+        if (location != "") {
+         
+            $.ajax({
+                method: "GET",
+                url: "/GetLocationData?location=" + location,
+                beforeSend: function () {
+                    $('#spinner').removeClass("hide");
+                },
+                success: function (response) {
+                    
+                    locationData = JSON.parse(response.data);
+
+                    map.setView([locationData.location.lat, locationData.location.lon], 5)
+
+                    console.log(locationData);
+
+                    var imagePath = "//cdn.weatherapi.com/weather/64x64/night/113.png"
+
+                    let popupHtml = `
+                        
+                        <div  >
+                            <div style="display:flex;flex-direction:row" >
+                                <h5 style="margin:auto" > <i> ${locationData.current.condition.text} </h5></i>  <img class="popupImage" src="${locationData.current.condition.icon}" >
+                             </div>   
+                           
+                            <hr>
+
+                            <p id="CelsiusFooter" class="popupFooter ${outputType == 1 ? "" : "hide"}" style="font-size:15px" >${locationData.current.temp_c} °C • ${locationData.current.wind_kph} km/h • ${locationData.current.wind_dir}</p>
+
+                            <p id="FarenheitFooter" class="popupFooter ${outputType == 2 ? "" : "hide"}" style="font-size:15px" >${locationData.current.temp_f} °F • ${locationData.current.wind_kph} mp/h • ${locationData.current.wind_dir}</p>
+
+
+                        </div>
+                        
+                    `
+                        ;
+
+                    L.popup()
+                        .setLatLng([locationData.location.lat, locationData.location.lon])
+                        .setContent(popupHtml)
+                        .openOn(map);
+                },
+                error: function (xhr, status, error) {
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'No Location Found',
+                        icon: 'error',
+                        confirmButtonText: 'Cool',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                },
+                complete: function () {
+                    $('#spinner').addClass("hide");
+                }
+            });
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+  
+
+}
+
+function selectCelsius() {
+    
+    outputType = 1;
+
+    $('#Celsius').addClass("metric-selected");
+    $('#Farenheit').removeClass("metric-selected");
+
+    $('#CelsiusFooter').removeClass("hide");
+    $('#FarenheitFooter').addClass("hide");
+
+}
+function selectFareinheit() {
+    
+    outputType = 2;
+
+    $('#Celsius').removeClass("metric-selected");
+    $('#Farenheit').addClass("metric-selected");
+
+    $('#CelsiusFooter').addClass("hide");
+    $('#FarenheitFooter').removeClass("hide");
+
+    
+}
+
+function reloadData() {
+
+    lookupLocation();
+
+    if (outputType == 1) {
+        selectCelsius();
+    } else {
+        selectFareinheit();
+    }
+}
